9bbfd67c2c1bdecd841b4411e3df6f4722c08a8a
BenjaminKarasik28 <karasik.benjamin28@gmail.com>
2019-09-25 15:40:48 -0400

    updating with js files

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,55 +1,92 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let loginButton = document.querySelector('#login')
-    let emailInput = document.querySelector('#username')
-    let passwordInput = document.querySelector('#password')
 
-    fetch("http://thesi.generalassemb.ly:8080/post/list", {
-        method: 'GET'
-    })
-    .then((response )=> {
-        return response.json();
-    })
-    .then((json) =>{
-        console.log(json)
-    })
     
-    loginButton.addEventListener('click', function(){
-        let email = emailInput.value
-        let password = passwordInput.value
-        console.log(email)
-        console.log(password)
+document.addEventListener("DOMContentLoaded", function(e) {
+    let loggedInFlag = false
+    loadPosts(loggedInFlag)
+    e.preventDefault();
+
+    
+    
+    document.querySelector("#login").addEventListener("click", function(e){
+        if(logInOnClick()){
+            console.log('YEAHAHAHAHAH')
+        }
+    });
+  });
+  
+  function signUpOnClick() {}
+  
+  function loadButtonsOnLogin(){
+
+  }
+
+
+  function logInOnClick() {
+    let emailInput = document.querySelector("#username").value;
+    let passwordInput = document.querySelector("#password").value;
+    console.log(`email: ${emailInput} password: ${passwordInput}`);
+    let loggin_token = logIn("venom@superhero.com", "venom");
+    console.log(loggin_token);
+    if (loggin_token) {
+      document.querySelector("#form").innerHTML = " ";
+      let index = emailInput.indexOf("@");
+      let profileName = emailInput.slice(0, index);
+      let linkToProfile = document.createElement("a");
+      linkToProfile.innerHTML = profileName;
+      linkToProfile.href = "#";
+      document.querySelector("#header").appendChild(linkToProfile);
+      console.log(linkToProfile);
+      return true
+    }
+    else return false
+
+    
+  }
+   
+function loadPosts (loggedInFlag){
+    let url  = `http://thesi.generalassemb.ly:8080/post/list`
+    getData(url)
+    .then(posts =>{
         
+        posts.forEach((post) => {
 
-        fetch("http://thesi.generalassemb.ly:8080/login", {
-            method: 'POST',
-            headers:{
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }, 
-            body: JSON.stringify({
-                email: email,
-                password: password,
-            })
-        })
-            .then((response )=> {
-                return response.json();
-            })
-            .then((json) =>{
+            let title = `Title: ${post.title}`
+            let postTitle = document.createElement('h1')
+            postTitle.textContent = title
+
+
+            let desc = post.description
+            let postDesc = document.createElement('p')
+            postDesc.textContent = desc
+
+            let username = ` by: ${post.user.username}`
+            console.log(username)
+            let postUsername = document.createElement('p')
+            postUsername.textContent = username
+            
+            
+
+            let commentOnAPost = document.createElement('button') 
+            
+                console.log('IM HERE')
+                let addComment = "add a comment"
                 
-                 document.querySelector("#form").innerHTML = " "
-                let index = email.indexOf('@')
-                let profileName = email.slice(0, index)
-                let linkToProfile = document.createElement('a')
-                linkToProfile.innerHTML = profileName
-                linkToProfile.href = "#"
-                document.querySelector("#header").appendChild(linkToProfile)
-                console.log(linkToProfile)
+                commentOnAPost.textContent = addComment
+               
+            
 
-                //console.log(json)
-            })
-            .catch(function(error){
-                alert(error)
-            })
+        
+            
+            document.querySelector('#main').appendChild(postTitle)
+            document.querySelector('#main').appendChild(postDesc)
+            document.querySelector('#main').appendChild(postUsername)
+
+            if(!loggedInFlag)
+            document.querySelector('#main').appendChild(commentOnAPost)
+            
+           
+
+        })
+        
     })
-      
-})
+}
