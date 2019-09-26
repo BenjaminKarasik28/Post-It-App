4460bf985556cfa5683a71c50d7d5c5e3f83a7c9
Qiming Chen <qimingchen91129@gmail.com>
2019-09-25 20:33:05 -0400

    qc post.html, css, js finished one post view

--- src/javascript/post.js
+++ src/javascript/post.js
@@ -2,21 +2,40 @@ document.addEventListener("DOMContentLoaded", function(e) {
   e.preventDefault();
   let tokenAvailable = checkTokenAvailable();
   console.log(`token availability: ${tokenAvailable}`);
-  document.getElementById("post-switcher-view").addEventListener("click", switchToView);
-  document.getElementById("post-switcher-create").addEventListener("click", switchToCreate);
+  document.getElementById("post-switcher-view-posts").addEventListener("click", switchToViewPosts);
+  document.getElementById("post-switcher-create").addEventListener("click", switchToCreatePost);
   document.getElementById("post-submit").addEventListener("click", createPostOnClick);
+
+  // add to every post
+  document.getElementById("a-post").addEventListener("click", switchToViewAPost);
 });
 
-async function switchToView() {
+function switchToViewAPost(e) {
+  alert("switchToViewAPost");
   document.getElementById("post-creation").style.display = "none";
-  document.getElementById("post-view").style.display = "block";
+  document.getElementById("posts-view").style.display = "none";
+  document.getElementById("one-post-view").style.display = "block";
+  //stopPropogation
+  let divOnePostView = document.getElementById("one-post-view");
+  let title = document.getElementById("a-post").children[0].innerText; //h3#a-post-title
+  let content = document.getElementById("a-post").children[1].innerText; //p#a-post-content
+  let meta = document.getElementById("a-post").children[2].innerText; //p#a-post-meta
+  divOnePostView.innerHTML = "<h1>" + title + "</h1>" + "<p>" + content + "</p>" + "<p>" + meta + "</p>";
+
+}
+
+async function switchToViewPosts() {
+  document.getElementById("post-creation").style.display = "none";
+  document.getElementById("posts-view").style.display = "block";
+  document.getElementById("one-post-view").style.display = "none";
   let userPosts = await listPosts().then(response => {
     console.log(response);
   });
 }
-function switchToCreate() {
+function switchToCreatePost() {
   document.getElementById("post-creation").style.display = "block";
-  document.getElementById("post-view").style.display = "none";
+  document.getElementById("posts-view").style.display = "none";
+  document.getElementById("one-post-view").style.display = "none";
 }
 
 function checkTokenAvailable() {
