fa0056d8ac7f07e08edda1530a26d5a63ca74560
BenjaminKarasik28 <karasik.benjamin28@gmail.com>
2019-09-26 14:11:42 -0400

    "newest"

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,30 +1,20 @@
+/*
+TO DO: 
+1. Create request to add a comment
+2. Create button with request to view all comments per post
+3. create delete post button
+4. add some search functionality
+
+*/
+
+
+
 
 document.addEventListener("DOMContentLoaded", function(e) {
     loadPosts()
     let logInButton = document.createElement('button')
-
+    loadLogin(logInButton)
     
-
-    if(localStorage.getItem("sessionToken")=== null || localStorage.getItem("sessionToken")=== ''  ) { //replace with token DEFINITELY
-        console.log("im here")
-        console.log(typeof(localStorage.getItem("sessionToken")))
-        
-        
-        logInButton.textContent = "log in"
-        document.querySelector("#login").innerHTML = `<input id = "username" placeholder="email">
-            <input id = "password" type= "password" placeholder="password" >`
-        logInButton.type = "submit"
-        logInButton.value = "Log in"
-        document.querySelector("#login").appendChild(logInButton)
-    }
-    else {
-        let userName = localStorage.getItem("email").split("@")[0]
-        let helloUsername = `Welcome back, ${userName}`
-        let welcomeBack = document.createElement("p")
-        welcomeBack.textContent = helloUsername
-        document.querySelector("#login").appendChild(welcomeBack)
-    }
-
     logInButton.addEventListener('click', function(e){
       
       loadUser()
@@ -33,12 +23,9 @@ document.addEventListener("DOMContentLoaded", function(e) {
    
 })
 
-
-
 function loadUser(){
 
     let emailInput = document.querySelector("#username").value
-
     let passwordInput = document.querySelector("#password").value
 
     console.log(emailInput)
@@ -60,22 +47,34 @@ function loadUser(){
         return response.json()
     })
     .then((json) => {
-        // console.log(json)
-        let token = json.token
         
-        console.log(token)
-        localStorage.setItem("email", emailInput)
-        localStorage.setItem("sessionToken", token)
-        //document.reload()
+        let token = json.token
+        if(token!==undefined){
+            console.log(token)
+            localStorage.setItem("email", emailInput)
+            localStorage.setItem("sessionToken", token)
+            location.reload();
+        }
+        else{
+            let loginError = document.createElement('p')
+            loginError.textContent = "Your email or password is incorrect"
+            loginError.style.color  = "red"
+            document.querySelector("#login").appendChild(loginError)
+            loginError.style.display = ''
+        }
         
         
     })
-    .catch(error =>{
+    .catch( () =>{
         alert(error)
     })
 
 }
+function loadCreatePostButton(){
+
+}
 function loadPosts(){
+
     fetch("http://thesi.generalassemb.ly:8080/post/list", {
         method: "GET"
     })
@@ -83,157 +82,83 @@ function loadPosts(){
         return response.json();
     })
     .then(posts =>{
-        posts.forEach((post)=>{
-            let title = `Title: ${post.title}`
-            let postTitle = document.createElement('h1')
-            postTitle.textContent = title
+       
+            posts.forEach((post)=>{
 
-            let desc = post.description
-            let postDesc = document.createElement('p')
-            postDesc.textContent = desc
+                let commentButton = document.createElement('button')
+                
+            
+                let title = `Title: ${post.title}`
+                let postTitle = document.createElement('h1')
+                postTitle.textContent = title
 
-            let username = ` by: ${post.user.username}`
-            let postUsername = document.createElement('p')
-            postUsername.textContent = username
+                let desc = post.description
+                let postDesc = document.createElement('p')
+                postDesc.textContent = desc
 
-            document.querySelector('#main').appendChild(postTitle)
-            document.querySelector('#main').appendChild(postDesc)
-            document.querySelector('#main').appendChild(postUsername)
+                let username = ` by: ${post.user.username}`
+                let postUsername = document.createElement('p')
+                postUsername.textContent = username
 
 
-        })
+                document.querySelector('#main').appendChild(postTitle)
+                document.querySelector('#main').appendChild(postDesc)
+                document.querySelector('#main').appendChild(postUsername)
+                
+                if(localStorage.getItem("sessionToken") !==null){
+                    
+                    commentButton.innerHTML = "Add a comment"
+                    document.querySelector('#main').appendChild(commentButton)
+                }
+
+                console.log(commentButton)
+                commentButton.addEventListener('click', ()=>{
+                    
+                    let commentDiv = document.createElement("div")
+                    let commentInput = document.createElement("input")
+                    let newCommentAdd = document.createElement("button")
+                    
+                    newCommentAdd.innerHTML = "Post Comment"
+                    commentInput.setAttribute("type", "text")
+                    postUsername.appendChild(commentDiv)
+                    postUsername.appendChild(commentInput)
+                    postUsername.appendChild(newCommentAdd)
+                    commentButton.style.display = "none"
+
+
+                })
+
+            
+            })
+        
+       
     })
 }
    
 
-    
-
-
-
-//             let commentOnAPost = document.createElement('button') 
-            
-//                 console.log('IM HERE')
-//                 let addComment = "add a comment"
-                
-//                 commentOnAPost.textContent = addComment
-            
-            
-
+function loadLogin(logInButton){
+    if(!localStorage.getItem("sessionToken") ) { 
+       
+        logInButton.textContent = "log in"
+        document.querySelector("#login").innerHTML = `<input id = "username" placeholder="email">
+            <input id = "password" type= "password" placeholder="password" >`
+        logInButton.type = "submit"
+        logInButton.value = "Log in"
+        document.querySelector("#login").appendChild(logInButton)
         
-            
-//             document.querySelector('#main').appendChild(postTitle)
-//             document.querySelector('#main').appendChild(postDesc)
-//             document.querySelector('#main').appendChild(postUsername)
-
-//             if(token!==null){
-//             document.querySelector('#main').appendChild(commentOnAPost)
-            
-//             }
-
-//         })
+    }
+    else {
+        let userName = localStorage.getItem("email").split("@")[0]
+        let helloUsername = `Welcome back, ${userName}`
+        let welcomeBack = document.createElement("p")
+        welcomeBack.textContent = helloUsername
+        document.querySelector("#login").appendChild(welcomeBack)
         
-//     })
-// }
+        let createNewPost = document.createElement("a")
+        createNewPost.textContent = "Create a new Post"
+        createNewPost.href = "../html/post.html"
+        document.querySelector("#login").appendChild(createNewPost)
+    }
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-// function onLogIn() {
-//     let div = document.querySelector("#form")
-//     div.innerHTML = 
-//     console.log('qwe');
-//     console.log(localStorage);
-    
-//     if(localStorage.getItem("email")){
-//         let span = document.createElement('span')
-//         span.innerText = `Hi, ${localStorage.getItem("email"). split('@')[0]}`
-//         div.appendChild(span)
-//     }
-//     else{
-//         console.log('ewq');
-        
-//         const button = document.createElement("button");
-//         button.innerText = 'log in'
-//         console.log(div);
-        
-//         button.addEventListener("click", function(e){
-//             console.log('input');
-//             logInOnClick();
-//         })
-//         div.innerHTML = ` <input id = "username" placeholder="email">
-//         <input id = "password" type= "password" placeholder="password" >
-//         `;
-//         console.log(div);
-        
-//         div.appendChild(button)
-//     }
-// }
-    
-// document.addEventListener("DOMContentLoaded", function(e) {
-    
-//     onLogIn();
-//     let token = sessionStorage.getItem('token')
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     /**
-//  * load dom
-//  * check local storage
-//  * call load login 
-//  * if key there, load name, else load screen to ask the user for their stuff, call load again
-//  * call load posts 
-//  * if key there have add comment button
-//  * also need delete comment
-//  * 
-//  * 
-//  * 
-//  */
-
-
-
-//     // //       let data = {
-//     // //         email: email,
-//     // //         password: pwd,
-//     // //       };')
-//     // let loggin_token = logIn("venom@superhero.com", "venom");
-//     // console.log(loggin_token);
-//     //
-
-//     // if (loggin_token) {
-//     //   document.querySelector("#form").innerHTML = " ";
-//     //   let index = emailInput.indexOf("@");
-//     //   let profileName = emailInput.slice(0, index);
-//     //   console.log(document.querySelector("#my-new-UN") + 'FIND ME BENJI')
-//     //   //let linkToProfile = document.querySelector("#my-new-UN");
-//     //   let linkToProfile = document.createElement('a');
-//     //   linkToProfile.innerText = profileName;
-//     //   linkToProfile.href = "#";
-//     //   document.querySelector("#header").appendChild(linkToProfile);
-//     //   console.log(linkToProfile);
-//     //   return true
-//     // //}
-//     // }
-//     return false 
-//   }
--- src/javascript/post.js
+++ src/javascript/post.js
@@ -1,17 +1,106 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-    e.preventDefault();
-    document.getElementById("post-submit").addEventListener("click", createPostOnClick);
-  });
-  
-  function createPostOnClick() {
-    try {
-      let title = document.querySelector("#post-title").value;
-      let dscrpt = document.querySelector("#post-content").value;
-      console.log(title);
-      console.log(dscrpt);
-      createPost(title, dscrpt);
-    } catch (err) {
-      console.log(err);
-    }
+  e.preventDefault();
+  let tokenAvailable = checkTokenAvailable();
+  if (tokenAvailable) {
+    document.getElementById("form-to-login").style.display = "none";
+    document.getElementById("logined-username").innerText = sessionStorage.getItem("username");
+  } else {
+    document.getElementById("form-logined").style.display = "none";
   }
-  
+  console.log(`token availability: ${tokenAvailable}`);
+  document.getElementById("post-switcher-view-posts").addEventListener("click", switchToViewPosts);
+  document.getElementById("post-switcher-create").addEventListener("click", switchToCreatePost);
+  document.getElementById("post-submit").addEventListener("click", createPostOnClick);
+  document.getElementById("comment-submit").addEventListener("click", createCommentOnClick);
+
+  // add to every post
+  document.getElementById("list-post").addEventListener("click", switchToViewAPost);
+});
+
+// TODO: test
+function createCommentOnClick() {
+  try {
+    let comment = document.querySelector("#comment-content").value;
+    console.log(comment);
+    createComment(comment);
+  } catch (err) {
+    console.log(err);
+  }
+}
+function switchToViewAPost(e) {
+  alert("switchToViewAPost");
+  document.getElementById("post-creation").style.display = "none";
+  document.getElementById("posts-list").style.display = "none";
+  document.getElementById("post-view").style.display = "block";
+  //stopPropogation
+  let divOnePostView = document.getElementById("list-post");
+  let title = document.getElementById("list-post").children[0].innerText; //h3#list-post-title
+  let content = document.getElementById("list-post").children[1].innerText; //p#list-post-content
+  let meta = document.getElementById("list-post").children[2].innerText; //p#list-post-meta
+  console.log(title);
+}
+
+async function switchToViewPosts() {
+  document.getElementById("post-creation").style.display = "none";
+  document.getElementById("posts-list").style.display = "block";
+  document.getElementById("post-view").style.display = "none";
+  let userPosts = await listPosts().then(response => {
+    displayUserPosts(response);
+  });
+}
+function displayUserPosts(data) {
+  let username = sessionStorage.getItem("username");
+  console.log(username);
+  console.log(data);
+  let filteredData = data.filter(data => data.user.username === username);
+
+  let targetElement = document.getElementById("posts-list");
+  targetElement.innerHTML = "";
+  for (let i = 0; i < filteredData.length; i++) {
+    let newItem = filteredData[i];
+    let title = newItem.title;
+    let content = newItem.description;
+    let meta = newItem.id;
+
+    let newDiv = document.createElement("div")
+    newDiv.setAttribute("id", "list-post");
+    let newTitle = document.createElement("h3");
+    newTitle.setAttribute("id", "list-post-title");
+    newTitle.innerText = title;
+    let newContent = document.createElement("p");
+    newContent.setAttribute("id", "list-post-content");
+    newContent.innerText = content;
+    let newMeta = document.createElement("p");
+    newMeta.setAttribute("id", "list-post-meta");
+    newMeta.innerText = "post id: " + meta;
+    newDiv.appendChild(newTitle);
+    newDiv.appendChild(newContent);
+    newDiv.appendChild(newMeta);
+    targetElement.appendChild(newDiv);
+  }
+}
+
+function switchToCreatePost() {
+  document.getElementById("post-creation").style.display = "block";
+  document.getElementById("posts-list").style.display = "none";
+  document.getElementById("post-view").style.display = "none";
+}
+
+function checkTokenAvailable() {
+  if (sessionStorage.getItem("token") === null) {
+    return false;
+  }
+  return true;
+}
+
+function createPostOnClick() {
+  try {
+    let title = document.querySelector("#post-title").value;
+    let dscrpt = document.querySelector("#post-content").value;
+    console.log(title);
+    console.log(dscrpt);
+    createPost(title, dscrpt);
+  } catch (err) {
+    console.log(err);
+  }
+}
