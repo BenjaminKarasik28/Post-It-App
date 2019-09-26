053bdecc2d2dced94deee193bee9261dd6f2e552
Ben <karasik.benjamin28@gmail.com>
2019-09-26 14:20:10 -0400

    working first page

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,3 +1,15 @@
+/*
+TO DO: 
+1. Create request to add a comment
+2. Create button with request to view all comments per post
+3. create delete post button
+4. add some search functionality
+
+*/
+
+
+
+
 document.addEventListener("DOMContentLoaded", function(e) {
     loadPosts()
     let logInButton = document.createElement('button')
@@ -48,17 +60,18 @@ function loadUser(){
             loginError.textContent = "Your email or password is incorrect"
             loginError.style.color  = "red"
             document.querySelector("#login").appendChild(loginError)
+            loginError.style.display = ''
         }
         
         
     })
-    .catch(error =>{
+    .catch( () =>{
         alert(error)
     })
 
 }
 function loadCreatePostButton(){
-    
+
 }
 function loadPosts(){
 
@@ -69,61 +82,83 @@ function loadPosts(){
         return response.json();
     })
     .then(posts =>{
-        
-        posts.forEach((post)=>{
+       
+            posts.forEach((post)=>{
 
-            
-            let commentButton = document.createElement('button')
-            
-        
-            let title = `Title: ${post.title}`
-            let postTitle = document.createElement('h1')
-            postTitle.textContent = title
-
-            let desc = post.description
-            let postDesc = document.createElement('p')
-            postDesc.textContent = desc
-
-            let username = ` by: ${post.user.username}`
-            let postUsername = document.createElement('p')
-            postUsername.textContent = username
-
-
-            document.querySelector('#main').appendChild(postTitle)
-            document.querySelector('#main').appendChild(postDesc)
-            document.querySelector('#main').appendChild(postUsername)
-            
-            if(localStorage.getItem("sessionToken") !==null){
+                let commentButton = document.createElement('button')
                 
-                commentButton.innerHTML = "Add a comment"
-                document.querySelector('#main').appendChild(commentButton)
-            }
+            
+                let title = `Title: ${post.title}`
+                let postTitle = document.createElement('h1')
+                postTitle.textContent = title
 
-        })
+                let desc = post.description
+                let postDesc = document.createElement('p')
+                postDesc.textContent = desc
+
+                let username = ` by: ${post.user.username}`
+                let postUsername = document.createElement('p')
+                postUsername.textContent = username
+
+
+                document.querySelector('#main').appendChild(postTitle)
+                document.querySelector('#main').appendChild(postDesc)
+                document.querySelector('#main').appendChild(postUsername)
+                
+                if(localStorage.getItem("sessionToken") !==null){
+                    
+                    commentButton.innerHTML = "Add a comment"
+                    document.querySelector('#main').appendChild(commentButton)
+                }
+
+                console.log(commentButton)
+                commentButton.addEventListener('click', ()=>{
+                    
+                    let commentDiv = document.createElement("div")
+                    let commentInput = document.createElement("input")
+                    let newCommentAdd = document.createElement("button")
+                    
+                    newCommentAdd.innerHTML = "Post Comment"
+                    commentInput.setAttribute("type", "text")
+                    postUsername.appendChild(commentDiv)
+                    postUsername.appendChild(commentInput)
+                    postUsername.appendChild(newCommentAdd)
+                    commentButton.style.display = "none"
+
+
+                })
+
+            
+            })
+        
+       
     })
 }
    
 
-
 function loadLogin(logInButton){
-    if(!localStorage.getItem("sessionToken") ) { //replace with token DEFINITELY
-        console.log("im here")
-        console.log(typeof(localStorage.getItem("sessionToken")))
-        
-        
+    if(!localStorage.getItem("sessionToken") ) { 
+       
         logInButton.textContent = "log in"
         document.querySelector("#login").innerHTML = `<input id = "username" placeholder="email">
             <input id = "password" type= "password" placeholder="password" >`
         logInButton.type = "submit"
         logInButton.value = "Log in"
         document.querySelector("#login").appendChild(logInButton)
+        
     }
     else {
         let userName = localStorage.getItem("email").split("@")[0]
-        let helloUsername = `Welcome back, ${userName[0].toUpperCase()}${userName.slice(1)}`
+        let helloUsername = `Welcome back, ${userName}`
         let welcomeBack = document.createElement("p")
         welcomeBack.textContent = helloUsername
         document.querySelector("#login").appendChild(welcomeBack)
+        
+        let createNewPost = document.createElement("a")
+        createNewPost.textContent = "Create a new Post"
+        createNewPost.href = "../html/post.html"
+        document.querySelector("#login").appendChild(createNewPost)
     }
 
 }
+
